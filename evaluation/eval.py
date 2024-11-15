1195041d18d71c77d5c27ee665ae5467867e1e15
Jiang241023 <jiangyueyang2023de@outlook.com>
2024-11-15 20:45:53 +0100

    Add files via upload

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,5 +1,34 @@
 import tensorflow as tf
+from metrics import ConfusionMatrix
+import logging
 
-def evaluate(model, checkpoint, ds_test, ds_info, run_paths):
+def evaluate(model, ds_test, ds_info, run_paths, checkpoint = False):
 
-    return
+    #if checkpoint:
+
+    metrics = ConfusionMatrix()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
+    evaluation_loss = tf.keras.metrics.Mean(name = 'evaluation_loss')
+    evaluation_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name = 'evaluation_accuracy')
+
+
+    for test_images, test_labels in ds_test:
+        predictions = model(test_images, training=False)
+        loss = loss_object(test_labels, predictions)
+
+        #update metrics
+        evaluation_loss.update_state(loss)
+        evaluation_accuracy.update_state(test_labels, predictions)
+        metrics.update_state(y_true = test_labels, y_pred = predictions)
+
+    final_evaluation_loss = evaluation_loss.result().numpy()
+    final_evaluation_accuracy = evaluation_accuracy.result().numpy()
+
+    logging.info(f"final_evaluation_loss: {final_evaluation_loss}")
+    logging.info(f"final_evaluation_accuracy: {final_evaluation_accuracy}")
+
+
+    return {
+        "final_evaluation_loss": final_evaluation_loss,
+        "final_evaluation_accuracy": final_evaluation_accuracy
+    }
--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,13 +1,32 @@
+import gin
 import tensorflow as tf
 
+@gin.configurable
 class ConfusionMatrix(tf.keras.metrics.Metric):
 
     def __init(self, num_classes, name="confusion_matrix", **kwargs):
         super(ConfusionMatrix, self).__init__(name=name, **kwargs)
         # ...
+        num_classes = num_classes
+        self.confusion_matrix = self.add_weight(
+            name = 'confusion_matrix',
+            shape = (num_classes, num_classes),
+            initializer = 'zeros',
+            dtype = tf.int32
+        )
 
-    def update_state(self, y_true, y_pred, sample_weight=None):
+    def update_state(self, y_true, y_pred, sample_weight = None):
         # ...
+        y_true = tf.cast(y_true, tf.int32)
+        y_pred = tf.cast(y_pred, tf.int32)
+
+        # calculate the confusion matrix
+        result = tf.math.confusion_matrix(y_true, y_pred, num_classes = self.confusion_matrix, dtype = tf.int32)
+
+        # update the existing confusion matrix by adding the values to it
+        self.confusion_matrix.assign_add(result)
+
 
     def result(self):
         # ...
+        return self.confusion_matrix
